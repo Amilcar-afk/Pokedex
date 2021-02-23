283c42e8f261b5f374d6eac7b80b4f789ab3618d
Fernandes <fernandesamilcar28@gmail.com>
2021-02-23 19:56:50 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+        <?php
+            include('includes/header.php');
+        ?>
+        <title>Accueil</title>
+        <link href="css/index.css" rel="stylesheet">
+        <body>
+        <main>
+            <img id="pika" src="images/pikachu.png">
+            <h1>bienvenue sur le pokedex de l'esgi !</h1>
+        </main>
+        </body>
+        <?php
+            include('includes/footer.php');
+        ?>
+</html>
